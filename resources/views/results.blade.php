4cc7696a10502486b7bd5e317abb88eea59a4865
Stephen Rees-Carter <stephen@rees-carter.net>
2020-05-17 11:26:02 +1000

    Improve layout slightly

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -10,51 +10,59 @@
     ];
     ?>
 
-    <h1>Recent Requests</h1>
     <table>
         <tr>
-            <th>Cookie</th>
-            @foreach($test->recent as $method => $matches)
-                <th>{{ $method }}</th>
-            @endforeach
-        </tr>
-        @foreach ($cookies as $cookie)
-            <tr>
-                <th><code>{{ $cookie }}</code></th>
-                @foreach($test->recent as $method => $matches)
-                    @if (in_array($cookie, $matches))
-                        <td><strong style="color: #0A0;">YES</strong></td>
-                    @else
-                        <td><strong style="color: #A00;">NO</strong></td>
-                    @endif
-                @endforeach
-            </tr>
-        @endforeach
-        <tr>
-        </tr>
-    </table>
-
-    <h1>Delayed Requests</h1>
-    <table>
-        <tr>
-            <th>Cookie</th>
-            @foreach($test->delayed as $method => $matches)
-                <th>{{ $method }}</th>
-            @endforeach
-        </tr>
-        @foreach ($cookies as $cookie)
-            <tr>
-                <th><code>{{ $cookie }}</code></th>
-                @foreach($test->delayed as $method => $matches)
-                    @if (in_array($cookie, $matches))
-                        <td><strong style="color: #0A0;">YES</strong></td>
-                    @else
-                        <td><strong style="color: #A00;">NO</strong></td>
-                    @endif
-                @endforeach
-            </tr>
-        @endforeach
-        <tr>
+            <td>
+                <h1>Recent Requests</h1>
+                <table>
+                    <tr>
+                        <th>Cookie</th>
+                        @foreach($test->recent as $method => $matches)
+                            <th>{{ $method }}</th>
+                        @endforeach
+                    </tr>
+                    @foreach ($cookies as $cookie)
+                        <tr>
+                            <th><code>{{ $cookie }}</code></th>
+                            @foreach($test->recent as $method => $matches)
+                                @if (in_array($cookie, $matches))
+                                    <td><strong style="color: #0A0;">YES</strong></td>
+                                @else
+                                    <td><strong style="color: #A00;">NO</strong></td>
+                                @endif
+                            @endforeach
+                        </tr>
+                    @endforeach
+                    <tr>
+                    </tr>
+                </table>
+            </td>
+            <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+            <td>
+                <h1>Delayed Requests</h1>
+                <table>
+                    <tr>
+                        <th>Cookie</th>
+                        @foreach($test->delayed as $method => $matches)
+                            <th>{{ $method }}</th>
+                        @endforeach
+                    </tr>
+                    @foreach ($cookies as $cookie)
+                        <tr>
+                            <th><code>{{ $cookie }}</code></th>
+                            @foreach($test->delayed as $method => $matches)
+                                @if (in_array($cookie, $matches))
+                                    <td><strong style="color: #0A0;">YES</strong></td>
+                                @else
+                                    <td><strong style="color: #A00;">NO</strong></td>
+                                @endif
+                            @endforeach
+                        </tr>
+                    @endforeach
+                    <tr>
+                    </tr>
+                </table>
+            </td>
         </tr>
     </table>
 @endsection
