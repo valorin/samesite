5eda753a1f7e123715d5f39fbebf04998609f131
Stephen Rees-Carter <stephen@rees-carter.net>
2020-05-26 21:24:57 +1000

    Enforce https on homepage

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'home');
+Route::get('/', function (\Illuminate\Http\Request $request) {
+    if (! $request->isSecure()) {
+        return redirect()->secure('/', 301);
+    }
+
+    return view('home');
+});
 
 Route::get('setup/start', 'SetupController@start');
 Route::get('setup/shared', 'SetupController@shared');
